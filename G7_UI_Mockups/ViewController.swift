92e0a59718d0b11e877495c8312648b30eabb657
Jay <amandagurns@hotmail.ca>
2025-02-20 12:44:25 -0500

    added main screen styling

---
+++ G7_UI_Mockups/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  G7_UI_Mockups
+//
+//  Created by Amanda Gurney on 2025-02-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
